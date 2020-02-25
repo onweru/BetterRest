19ac0f2697df61381e147e7c4cbd2e03fe5278ce
weru <onewesh@gmail.com>
2020-02-25 23:55:06 +0300

    save work

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,13 +9,82 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
-}
+    
+  @State private var wakeUp = defaultWakeTime
+  @State private var sleepAmount = 8.0
+  @State private var coffeeAmount = 1
+  
+  @State private var alertTitle = ""
+  @State private var alertMessage = ""
+  @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+     var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+  }
+    
+  func calculateBedtime() {
+      let model = SleepCalculator()
+    
+      let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+      
+      let hour = (components.hour ?? 0) * 60 * 60
+      let minute = (components.minute ?? 0) * 60
+      
+      do {
+          let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+          
+          let sleepTime = wakeUp - prediction.actualSleep
+          
+          let formatter = DateFormatter()
+          formatter.timeStyle = .short
+          
+          alertTitle = "Your ideal bedtime is ..."
+          alertMessage = formatter.string(from: sleepTime)
+      } catch {
+          alertTitle = "Error"
+          alertMessage = "Sorry, there was a problem calculating your bedtime."
+      }
+      
+      showingAlert = true
+  }
+    
+  var body: some View {
+      NavigationView {
+        Form {
+            DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                .labelsHidden()
+                .datePickerStyle(WheelDatePickerStyle())
+          
+            Text("Desired amount of sleep")
+              .font(.headline)
+          
+          Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+              Text("\(sleepAmount, specifier: "%g")")
+          }
+           Text("Daily coffee intake")
+                  .font(.headline)
+              
+            Stepper(value: $coffeeAmount, in: 1...20) {
+                Text("\(coffeeAmount) cup\( coffeeAmount == 1 ? "": "s")")
+            }
+        }
+        .navigationBarTitle("BetterRest")
+          .navigationBarItems(trailing: Button(action: calculateBedtime) {
+              Text("Calculate")
+          })
+              .alert(isPresented: $showingAlert) {
+                  Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("Ok")))
+          }
+        }
+      }
+  }
+
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
