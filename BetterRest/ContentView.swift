cfa847daceafd6117391526592757511be02153e
weru <onewesh@gmail.com>
2020-02-26 14:44:21 +0300

    update

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -13,43 +13,14 @@ struct ContentView: View {
   @State private var wakeUp = defaultWakeTime
   @State private var sleepAmount = 8.0
   @State private var coffeeAmount = 1
-  
-  @State private var alertTitle = ""
-  @State private var alertMessage = ""
-  @State private var showingAlert = false
     
-    static var defaultWakeTime: Date {
+  static var defaultWakeTime: Date {
      var components = DateComponents()
-        components.hour = 7
-        components.minute = 0
-        return Calendar.current.date(from: components) ?? Date()
+     components.hour = 7
+     components.minute = 0
+     return Calendar.current.date(from: components) ?? Date()
   }
     
-  func calculateBedtime() {
-      let model = SleepCalculator()
-    
-      let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-      
-      let hour = (components.hour ?? 0) * 60 * 60
-      let minute = (components.minute ?? 0) * 60
-      
-      do {
-          let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-          
-          let sleepTime = wakeUp - prediction.actualSleep
-          
-          let formatter = DateFormatter()
-          formatter.timeStyle = .short
-          
-          alertTitle = "Your ideal bedtime is ..."
-          alertMessage = formatter.string(from: sleepTime)
-      } catch {
-          alertTitle = "Error"
-          alertMessage = "Sorry, there was a problem calculating your bedtime."
-      }
-      
-      showingAlert = true
-  }
     
   var body: some View {
       NavigationView {
@@ -58,28 +29,56 @@ struct ContentView: View {
                 .labelsHidden()
                 .datePickerStyle(WheelDatePickerStyle())
           
-            Text("Desired amount of sleep")
-              .font(.headline)
-          
-          Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-              Text("\(sleepAmount, specifier: "%g")")
-          }
-           Text("Daily coffee intake")
-                  .font(.headline)
-              
+            
+            Section (header: Text("Desired amount of sleep")) {
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g")")
+                }
+
+            }
+
+            Section (header:                      Text("Daily coffee intake")) {
             Stepper(value: $coffeeAmount, in: 1...20) {
                 Text("\(coffeeAmount) cup\( coffeeAmount == 1 ? "": "s")")
             }
+            }
+            
+            Section (header: Text("Bedtime")) {
+                Text("\(calculateBedtime())")
+            }
         }
         .navigationBarTitle("BetterRest")
-          .navigationBarItems(trailing: Button(action: calculateBedtime) {
-              Text("Calculate")
-          })
-              .alert(isPresented: $showingAlert) {
-                  Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("Ok")))
-          }
         }
       }
+    
+    func calculateBedtime() -> String {
+        
+        var message = ""
+        
+        let model = SleepCalculator()
+        
+      
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            message = formatter.string(from: sleepTime)
+        } catch {
+            message = "Sorry, there was a problem calculating your bedtime."
+        }
+        
+        return message
+    }
+
   }
 
 
